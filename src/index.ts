1795b04baed47a95c7504c321962e7f1c0c23825
Chris Schardijn <chrismailat@gmail.com>
2024-12-22 19:46:53 +0100

    - more fixes to make npm run build work

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { SoundManager } from "./sound-manager/sound-manager";
-import { SoundStateInfo } from "./sound-manager/sound-state-info.interface";
 export { SoundState } from "./sound-manager/sound-state.interface";
 export type { Sound } from "./sound-manager/sound.interface";
--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,32 @@
-import { defineConfig } from 'vite';
-import * as path from 'path';
-import { fileURLToPath } from 'url';
-import dts from 'vite-plugin-dts';
+import { defineConfig } from "vite";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import dts from "vite-plugin-dts";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   server: {
-    open: '/index.html'
+    open: "/index.html",
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'SoundManagerTS',
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "SoundManagerTS",
       fileName: (format) => `sound-manager-ts.${format}.js`,
-      formats: ['es', 'umd']
+      formats: ["es", "umd"],
     },
     rollupOptions: {
       external: [],
       output: {
-        globals: {}
-      }
+        globals: {},
+      },
     },
   },
   plugins: [
     dts({
       insertTypesEntry: true,
-    })
-  ]
+    }),
+  ],
 });
