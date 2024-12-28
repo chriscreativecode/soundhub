41c6c570e16adcf8f8b77111650523e0c7cdeedf
Chris Schardijn <chrismailat@gmail.com>
2024-12-28 10:11:51 +0100

    - fixed a ton of issues! Like: - added the volume control - refactored the isPlaying and isPaused logic - fix pause/resume logic - added preload spinner in preload demo sounds - fix fade in - fix fade out.

--- src/sound-manager/sound-event.interface.ts
+++ src/sound-manager/sound-event.interface.ts
@@ -3,6 +3,7 @@ import { SoundEventsEnum } from "./sound-events.enum";
 export interface SoundEvent {
     type: SoundEventsEnum;
     soundId: string;
-    timestamp: number;
+    timestamp?: number;
+    currentTime?: number;
     error?: Error;
   }
