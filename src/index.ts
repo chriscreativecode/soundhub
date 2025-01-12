9c87d06ec5befad01becd19b7a10c0b5c9a70b13
Chris Schardijn <chrismailat@gmail.com>
2025-01-12 18:47:34 +0100

    -Fixed type declarations to prevent duplicate exports with incremental suffixes (_2) -Reorganized types into dedicated folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
 export { SoundManager } from './sound-manager/sound-manager';
-export { SoundState } from './types/types';
-export type { 
-  Sound,
-  SoundManagerConfig,
-  PlaySoundOptions,
-  SoundEvent,
-  SoundEventsEnum
-} from './types/types';
+export * from './types';  // This will exports all types in the folder types
